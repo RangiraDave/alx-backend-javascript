1f527b239fb2725a14021903af6b675c4d08aab3
RangiraDave <rangiradev666@gmail.com>
2024-05-27 18:22:49 +0200

    Condensed using default values in function signature

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,3 +1,4 @@
 export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
+  // eslint-disable-next-line no-unused-vars, no-shadow
   return initialNumber + expansion201989 + expansion2019;
 }
