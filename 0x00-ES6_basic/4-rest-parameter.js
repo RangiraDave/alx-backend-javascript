735f569b640a498f03b58d858c980f4bf769e700
RangiraDave <rangiradev666@gmail.com>
2024-05-27 18:41:48 +0200

    added a comment for eslint

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,5 +1,5 @@
 export default function returnHowManyArguments(...args) {
-  // Returning the number using rest parameter syntax.
+  // eslint-disable-next-line no-unused-vars, no-shadow Returning the number using rest parameter syntax.
   let i = 0;
   for (const temp of args) {
     i++;
