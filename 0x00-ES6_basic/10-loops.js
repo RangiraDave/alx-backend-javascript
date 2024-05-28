5f3ba89c8f5db84ea27bcbc8abb34d83d95b4a78
RangiraDave <rangiradev666@gmail.com>
2024-05-28 10:11:43 +0200

    incremented index to see the corresponding index to value

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,9 @@
 export default function appendToEachArrayValue(array, appendString) {
+  let index = 0;
   for (let value of array) {
-    const index = array.indexOf(value);
+    // const index = array.indexOf(value);
     array[index] = appendString + value;
+    index++;
   }
 
   return array;
