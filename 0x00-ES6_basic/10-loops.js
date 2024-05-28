e062f12dcfbf6183ae6c894287bb1777063ae977
RangiraDave <rangiradev666@gmail.com>
2024-05-28 10:14:46 +0200

    Used const instead of let

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,6 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
   let index = 0;
-  for (let value of array) {
+  for (const value of array) {
     // const index = array.indexOf(value);
     array[index] = appendString + value;
     index++;
