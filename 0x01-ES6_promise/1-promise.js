1371a0d493ec468cd787c9b9aa6eb97607bbe3ec
RangiraDave <rangiradev666@gmail.com>
2024-05-29 11:12:03 +0200

    Creating a promise according to the argument given

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,9 @@
+import default function getFullResponseFromAPI(success) {
+  const promise = new Promise((resolve, reject) => {
+    if (success) {
+      resolve({'status': 200, 'body': 'Success'});
+    } else {
+      reject("The fake API is not working currently");
+    }
+  });
+}
