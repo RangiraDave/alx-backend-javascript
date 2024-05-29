d49613a2450ff61837a2b6fd8f58466ead78f98d
RangiraDave <rangiradev666@gmail.com>
2024-05-29 21:38:37 +0200

    Script to return resolve promise object

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,6 +1,6 @@
 // Script to return resolve promise object
 export default function signUpUser(firstName, lastName) {
-  return new Promise ((resolve) => {
+  return new Promise((resolve) => {
     resolve({ firstName, lastName });
   });
 }
