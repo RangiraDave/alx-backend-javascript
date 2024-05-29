8ef827073618a51004012a01036bdad617680390
RangiraDave <rangiradev666@gmail.com>
2024-05-29 23:14:48 +0200

    try and cath on a function given as argument

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,1 +1,13 @@
-sdssdsd
+import { uploadPhoto, createUser } from './utils.js';
+
+export default async function asyncUploadUser() {
+  try {
+    return Promise.resolve({
+        photo: await uploadPhoto(),
+        user: await createUser()
+    });
+  }
+  catch (error) {
+    return Promise.resolve({ photo: null, user: null });
+  }
+}
