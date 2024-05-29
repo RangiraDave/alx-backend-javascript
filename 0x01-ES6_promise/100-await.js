d83a562112298b708ac2f0cb7365dbe74e8b105d
RangiraDave <rangiradev666@gmail.com>
2024-05-29 23:17:22 +0200

    await and async on a function given as argument

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -3,8 +3,8 @@ import { uploadPhoto, createUser } from './utils.js';
 export default async function asyncUploadUser() {
   try {
     return Promise.resolve({
-        photo: await uploadPhoto(),
-        user: await createUser()
+      photo: await uploadPhoto(),
+      user: await createUser()
     });
   } catch (error) {
     return Promise.resolve({ photo: null, user: null });
