850f3155327b979bba61310af50cd3de212d944b
RangiraDave <rangiradev666@gmail.com>
2024-05-29 11:00:41 +0200

    Removed reject method

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,7 +1,4 @@
 export default function getResponseFromAPI() {
-  const promise = new Promise((resolve) => {
-    resolve("FIRST PROMISE.")
-  });
-
+  const promise = new Promise((resolve) => {resolve("FIRST PROMISE.")});
   return promise;
 }
