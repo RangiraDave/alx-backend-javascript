26fce4877e2a6308759c846be235b81ce0862b3c
RangiraDave <rangiradev666@gmail.com>
2024-05-29 20:26:33 +0200

    Logged body, firstname and lastname on the console

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,14 @@
+import { uploadPhoto, createUser } from './utils.js';
+
+function handleProfileSignup() {
+    Promise.all([uploadPhoto(), createUser()])
+        .then((values) => {
+            const [photo, user] = values;
+            console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+        })
+        .catch(() => {
+            console.log('Signup system offline');
+        });
+}
+
+export default handleProfileSignup;
