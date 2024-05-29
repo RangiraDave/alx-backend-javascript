019b88c55ba73515a3499637c31e094c8b114c20
RangiraDave <rangiradev666@gmail.com>
2024-05-29 22:27:58 +0200

    Using .race

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,3 +1,4 @@
+// eslint-disable space-before-function-paren
 export default function loadBalancer(chinaDownload, USDownload) {
-  return Promise.race([chinaDownload, USDownload])
+  return Promise.race([chinaDownload, USDownload]);
 }
