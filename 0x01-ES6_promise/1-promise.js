8cebb5255d9e6ea4b8fc5def2210ea5ecea4f98a
RangiraDave <rangiradev666@gmail.com>
2024-05-29 11:25:13 +0200

    Replaced import with export

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,11 +1,11 @@
 export default function getFullResponseFromAPI(success) {
   const promise = new Promise((resolve, reject) => {
     if (success) {
-      resolve({'status': 200, 'body': 'Success'});
+      return resolve({'status': 200, 'body': 'Success'});
     } else {
-      reject("The fake API is not working currently");
+      return reject("The fake API is not working currently");
     }
   });
 
-  return promise;
+  //return promise;
 }
