ce25dac9f3dba7cc2645a61bc481e59625dd979f
RangiraDave <rangiradev666@gmail.com>
2024-05-30 20:14:54 +0200

    Airport class that uses Symbol

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,14 @@
+// Implementation of Airport class with the following methods:
+// name and code.
+// The default string description should return the airport code.
+
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  get [Symbol.toStringTag]() {
+    return `${this._code}`;
+  }
+}
