c72c4462337bea8ee40ad89622570f7b529560aa
RangiraDave <rangiradev666@gmail.com>
2024-05-30 12:21:55 +0200

    Removed class comment

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -3,7 +3,6 @@
 import ClassRoom from './0-classroom.js';
 
 export default function initializeRooms() {
-  // returns 3 ClassRoom instances
   return [
     new ClassRoom(19),
     new ClassRoom(20),
