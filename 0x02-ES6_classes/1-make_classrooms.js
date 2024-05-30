34a03bdc2068fa98fec46119eadf2d0a3e704b82
RangiraDave <rangiradev666@gmail.com>
2024-05-30 12:17:02 +0200

    Removed indentation

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,6 +1,6 @@
 // Script to create a function that returns 3 CLassRoom instances
 
-import ClassRoom from "./0-classroom.js";
+import ClassRoom from './0-classroom.js';
 
 export default function initializeRooms() {
   // returns 3 ClassRoom instances
