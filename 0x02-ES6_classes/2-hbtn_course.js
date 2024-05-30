3930f9790c705b5687584c9bc04b34c5cf211c54
RangiraDave <rangiradev666@gmail.com>
2024-05-30 12:44:39 +0200

    Corrected lint error

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -5,9 +5,11 @@ export default class HolbertonCourse {
     if (typeof name !== 'string') {
       throw new TypeError('Name must be a string');
     }
+
     if (typeof length !== 'number') {
       throw new TypeError('Length must be a number');
     }
+
     if (typeof students !== 'object') {
       throw new TypeError('Students must be an array');
     }
