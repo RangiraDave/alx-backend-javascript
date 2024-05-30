e7fa482193d00161827dcbe9aa9514af45b7ed31
RangiraDave <rangiradev666@gmail.com>
2024-05-30 12:37:10 +0200

    Corrected lint error

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,6 +1,6 @@
 // Script to create a function that returns 3 CLassRoom instances
 
-import ClassRoom from './0-classroom.js';
+import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
   return [
