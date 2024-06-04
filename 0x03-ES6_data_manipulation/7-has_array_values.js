f8ed57803922cc2a04e17c45fd61b6486ef2f8be
RangiraDave <rangiradev666@gmail.com>
2024-06-04 10:55:48 +0200

    used for ... of loop

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,4 +1,10 @@
 // A function to return a boolean value if all the elements in a set are present in the array of values.
 export default function hasValuesFromArray(set, array) {
-  return array.every((value) => set.has(value));
+  //return array.every((value) => set.has(value));
+  for (let value of array) {
+    if (!set.has(value)) {
+      return false;
+    }
+  }
+  return true;
 }
