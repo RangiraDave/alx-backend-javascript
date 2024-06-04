f7038c0e9b7c556cceb3988cdb5bff98a73fc9d2
RangiraDave <rangiradev666@gmail.com>
2024-06-04 10:10:59 +0200

    put if on one line to correct eslint

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,7 +1,5 @@
 export default function getListStudentIds(array) {
-  if (!Array.isArray(array)) {
-    return [];
-  }
+  if (!Array.isArray(array)) return [];
 
   return array.map(obj => obj.id);
 }
