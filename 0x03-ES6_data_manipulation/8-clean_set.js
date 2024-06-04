e33eaab28b72193f37601bf46174b070dc4531e9
RangiraDave <rangiradev666@gmail.com>
2024-06-04 11:10:07 +0200

    Function that returns a clean set using filter, map, slice and join

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,16 +1,10 @@
-// Function that takes two arguments; set and startString.
-// It then returns a new set with all values removed the part matching the 'startString'.
 export default function cleanSet(set, startString) {
-    // If the set is not an instance of Set, return an empty set.
-    if (!(set instanceof Set)) {
-        return new Set();
-    }
-    
-    // If the startString is not a string, return the set.
-    if (typeof startString !== 'string') {
-        return set;
-    }
-    
-    // Return a new set with all values removed the part matching the 'startString'.
-    return new Set([...set].filter((value) => !value.startsWith(startString)));
+  if (!(set instanceof Set)) {
+    return '';
+  }
+
+  return [...set]
+    .filter((value) => value.startsWith(startString))
+    .map((value) => value.slice(startString.length))
+    .join('-');
 }
