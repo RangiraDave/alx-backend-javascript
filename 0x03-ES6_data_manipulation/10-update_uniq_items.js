a5d7eb74d8afe5e510729c1b424f55a2609f23b2
RangiraDave <rangiradev666@gmail.com>
2024-06-04 11:21:23 +0200

    Function to uprade certain map values

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,7 +1,9 @@
+// Update the value of the key in a Map object.
 export default function updateUniqueItems(map) {
   if (!(map instanceof Map)) {
     throw new Error('Cannot process');
   } else {
+    // Iterate over the map and update the value of the key if it is equal to 1.
     for (const [key, value] of map) {
       if (value === 1) {
         map.set(key, 100);
