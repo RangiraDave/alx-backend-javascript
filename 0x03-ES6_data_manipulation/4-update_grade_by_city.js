2502ae7714b735469b9938865d4c1e38d895a6c2
RangiraDave <rangiradev666@gmail.com>
2024-06-04 10:32:20 +0200

    added comments

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,9 +1,10 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
+  // used filter to get the students in the city
   return students.filter((student) => student.location === city)
     .map((student) => {
       const grade = newGrades.filter((newGrade) => newGrade.studentId === student.id);
       const newGrade = grade.length > 0 ? grade[0].grade : 'N/A';
 
-      return { ...student, grade: newGrade };
+      return { ...student, grade: newGrade }; // used spread operator to copy the student object and add the new grade
     });
 }
