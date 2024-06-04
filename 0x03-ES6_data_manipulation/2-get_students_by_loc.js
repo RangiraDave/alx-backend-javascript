e0fa5a9f00af7b0ff26fe827fdf976ee704dba42
RangiraDave <rangiradev666@gmail.com>
2024-06-04 10:23:43 +0200

    Fitered students of a certain city

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,5 +1,7 @@
 export default function getStudentsByLocation(array, city) {
-  if (!Array.isArray(array)) return [];
+  if (!Array.isArray(array)) {
+    return [];
+  }
 
   return array.filter(obj => obj.location === city);
 }
