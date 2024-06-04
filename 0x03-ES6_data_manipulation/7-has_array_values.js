fdf656563865d0c8751c9419ab591394df0754e6
RangiraDave <rangiradev666@gmail.com>
2024-06-04 10:59:48 +0200

    correcting lint

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,6 +1,4 @@
 // A function to return a boolean value if all the elements in a set are present in the array of values.
 export default function hasValuesFromArray(set, array) {
-  //return array.every((value) => set.has(value));
-  for (let value of array) if (!set.has(value)) return false;
-  return true;
+  return array.every((value) => set.has(value));
 }
