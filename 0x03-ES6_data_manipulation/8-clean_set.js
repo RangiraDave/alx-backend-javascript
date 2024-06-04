c8afa7bd44c62ca8b5fcdb7b1028f804fd76c635
RangiraDave <rangiradev666@gmail.com>
2024-06-04 11:12:09 +0200

    Function that returns a clean set using filter, map, slice and join

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,4 @@
 export default function cleanSet(set, startString) {
-  if (!(set instanceof Set)) {
-    return '';
-  }
-
   return [...set]
     .filter((value) => value.startsWith(startString))
     .map((value) => value.slice(startString.length))
