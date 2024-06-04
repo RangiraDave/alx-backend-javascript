5231bbf044766bd0541cd2cf9e88d7973680bc1c
RangiraDave <rangiradev666@gmail.com>
2024-06-04 11:33:34 +0200

    Function to truck weak map

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,9 +1,11 @@
 export const weakMap = new WeakMap();
+
 export default function queryAPI(endpoint) {
+  // Increment the number of queries to the passed endpoint    
   if (!weakMap.has(endpoint)) {
     weakMap.set(endpoint, 1);
   } else {
     weakMap.set(endpoint, weakMap.get(endpoint) + 1);
   }
-  if (weakMap.get(endpoint) >= 5) throw Error('Endpoint load is high');
+  if (weakMap.get(endpoint) >= 5) throw new Error('Endpoint load is high');
 }
